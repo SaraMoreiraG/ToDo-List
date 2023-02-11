a083343ec0eabca482406be63e9de983fc08f752
SaraMoreiraG <sara_moreira@hotmail.es>
2023-02-11 19:51:35 +0000

    ToDo List

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,19 @@
 // Your code here
+var ul = document.querySelector('ul');
+var newTask = document.querySelector('input');
+
+newTask.addEventListener("change", function(event){
+    let newLi = document.createElement('li');
+    newLi.innerHTML = `<span><i class="fa fa-trash"></i></span> ` + event.target.value;
+    ul.appendChild(newLi);
+    newLi.addEventListener("click", function(event){
+        ul.removeChild(newLi); 
+    });
+});
+
+let span = document.querySelectorAll('span');
+
+span.forEach(element => element.addEventListener("click", function(event){
+    var listItem = this.parentNode;
+    ul.removeChild(listItem); 
+}));
